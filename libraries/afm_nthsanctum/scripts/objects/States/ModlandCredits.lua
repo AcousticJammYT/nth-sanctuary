0a18c2bf180ef22af4e147bf2c5d67da10964bd2
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-11-15 02:22:31 -0700

    Ensure Kristal credits remain updated

--- libraries/afm_nthsanctum/scripts/objects/States/ModlandCredits.lua
+++ libraries/afm_nthsanctum/scripts/objects/States/ModlandCredits.lua
@@ -4,7 +4,7 @@ local ModlandCredits, super = Class(MainMenuCredits)
 
 function ModlandCredits:init(menu)
     super.init(self,menu)
-    self.pages = {
+    self.pages = TableUtils.merge({
         {
             "#th Sanctuary",
             {
@@ -34,78 +34,7 @@ function ModlandCredits:init(menu)
                 ""
             }
         },
-        {
-            "Kristal Engine",
-            {
-                { "Lead Developers", COLORS.silver },
-                "NyakoFox",
-                "SylviBlossom",
-                "vitellary",
-                "",
-                { "Assets", COLORS.silver },
-                "Toby Fox",
-                "Temmie Chang",
-                "DELTARUNE team",
-                "",
-                "",
-            },
-            {
-                { "GitHub Contributors", COLORS.silver },
-                "Abbe",
-                "AcousticJamm",
-                "Agent 7",
-                "AlexGamingSW",
-                "Archie-osu",
-                "Azrael",
-                "Bor",
-                "CosmicPikachu001",
-                "Dobby233Liu"
-            }
-        },
-        {
-            "Kristal Engine",
-            {
-                { "GitHub Contributors", COLORS.silver },
-                "Elioze",
-                "FireRainV",
-                "HUECYCLES",
-                "Hyperboid",
-                "isakube",
-                "J.A.R.U.",
-                "Lionmeow",
-                "Luna",
-                "MCdeDaxia"
-            },
-            {
-                { "GitHub Contributors", COLORS.silver },
-                "MrOinky",
-                "NelleMonelle",
-                "Nextop",
-                "nightpool",
-                "prokube",
-                "Simbel",
-                "sjl057",
-                "skarph",
-                "SuperOfSrb2"
-            }
-        },
-        {
-            "Kristal Engine",
-            {
-                { "GitHub Contributors", COLORS.silver },
-                "TFLTV",
-                "Verozity",
-                "WIL-TZY",
-                "YeetusSnoopy",
-                "",
-                "",
-                "",
-                "",
-                "",
-                ""
-            }
-        }
-    }
+    }, self.pages)
 end
 
 function ModlandCredits:onKeyPressed(key)
