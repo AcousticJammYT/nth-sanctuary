c82a6c0bded56001e43ba31bb8fb08ae0b9289e2
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-01-23 17:34:33 -0700

    Update study.lua

--- scripts/world/cutscenes/study.lua
+++ scripts/world/cutscenes/study.lua
@@ -1,9 +1,10 @@
 return {
     fountain = function(cutscene, event)
-		if event.interact_count == 1 then
+		if not Game:getFlag("interactedWithFountain", false) then
 			cutscene:text("* (It's a pool of water. ...[wait:5] huh?)")
 			cutscene:text("* (A feather drifted into it...[wait:5] and turned into coins!)")
 			cutscene:text("* (Throw something inside?)")
+			Game:setFlag("interactedWithFountain", true)
 		else
 			cutscene:text("* (It's a pool of water. Throw something in?)")		
 		end
