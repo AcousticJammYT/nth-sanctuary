2da571f08a7b40fe38d6cc9e70abb01a522e1073
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-01-23 20:46:25 -0800

    New Watersource Tileset

---
+++ scripts/world/tilesets/WatersourceAnimated.lua
@@ -0,0 +1,107 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.2",
+  name = "WatersourceAnimated",
+  class = "",
+  tilewidth = 20,
+  tileheight = 20,
+  spacing = 0,
+  margin = 0,
+  columns = 5,
+  image = "../../../assets/sprites/tilesets/WatersourceAnimated.png",
+  imagewidth = 100,
+  imageheight = 80,
+  objectalignment = "unspecified",
+  tilerendersize = "grid",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 20,
+    height = 20
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 20,
+  tiles = {
+    {
+      id = 5,
+      animation = {
+        {
+          tileid = 5,
+          duration = 125
+        },
+        {
+          tileid = 6,
+          duration = 125
+        },
+        {
+          tileid = 7,
+          duration = 125
+        },
+        {
+          tileid = 8,
+          duration = 125
+        },
+        {
+          tileid = 9,
+          duration = 125
+        }
+      }
+    },
+    {
+      id = 10,
+      animation = {
+        {
+          tileid = 10,
+          duration = 125
+        },
+        {
+          tileid = 11,
+          duration = 125
+        },
+        {
+          tileid = 12,
+          duration = 125
+        },
+        {
+          tileid = 13,
+          duration = 125
+        },
+        {
+          tileid = 14,
+          duration = 125
+        }
+      }
+    },
+    {
+      id = 15,
+      animation = {
+        {
+          tileid = 15,
+          duration = 125
+        },
+        {
+          tileid = 16,
+          duration = 125
+        },
+        {
+          tileid = 17,
+          duration = 125
+        },
+        {
+          tileid = 18,
+          duration = 125
+        },
+        {
+          tileid = 19,
+          duration = 125
+        }
+      }
+    }
+  }
+}
