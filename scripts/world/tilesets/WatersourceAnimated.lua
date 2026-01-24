0bdc3259ef2ff34f94539087618f68721f727e34
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-01-24 02:30:47 -0800

    Just please work this time

--- scripts/world/tilesets/WatersourceAnimated.lua
+++ scripts/world/tilesets/WatersourceAnimated.lua
@@ -1,7 +1,11 @@
 return {
   version = "1.10",
   luaversion = "5.1",
+<<<<<<< Updated upstream
   tiledversion = "1.10.2",
+=======
+  tiledversion = "1.11.2",
+>>>>>>> Stashed changes
   name = "WatersourceAnimated",
   class = "",
   tilewidth = 20,
@@ -11,7 +15,11 @@ return {
   columns = 5,
   image = "../../../assets/sprites/tilesets/WatersourceAnimated.png",
   imagewidth = 100,
+<<<<<<< Updated upstream
   imageheight = 40,
+=======
+  imageheight = 60,
+>>>>>>> Stashed changes
   objectalignment = "unspecified",
   tilerendersize = "grid",
   fillmode = "stretch",
@@ -26,10 +34,17 @@ return {
   },
   properties = {},
   wangsets = {},
+<<<<<<< Updated upstream
   tilecount = 10,
   tiles = {
     {
       id = 5,
+=======
+  tilecount = 15,
+  tiles = {
+    {
+      id = 3,
+>>>>>>> Stashed changes
       animation = {
         {
           tileid = 5,
@@ -52,6 +67,204 @@ return {
           duration = 125
         }
       }
+<<<<<<< Updated upstream
+=======
+    },
+    {
+      id = 4,
+      animation = {
+        {
+          tileid = 10,
+          duration = 125
+        },
+        {
+          tileid = 11,
+          duration = 125
+        },
+        {
+          tileid = 12,
+          duration = 125
+        },
+        {
+          tileid = 13,
+          duration = 125
+        },
+        {
+          tileid = 14,
+          duration = 125
+        }
+      }
+    },
+    {
+      id = 5,
+      animation = {
+        {
+          tileid = 5,
+          duration = 125
+        },
+        {
+          tileid = 6,
+          duration = 125
+        },
+        {
+          tileid = 12,
+          duration = 125
+        },
+        {
+          tileid = 13,
+          duration = 125
+        },
+        {
+          tileid = 9,
+          duration = 125
+        },
+        {
+          tileid = 5,
+          duration = 125
+        },
+        {
+          tileid = 11,
+          duration = 125
+        },
+        {
+          tileid = 12,
+          duration = 125
+        },
+        {
+          tileid = 8,
+          duration = 125
+        },
+        {
+          tileid = 9,
+          duration = 125
+        },
+        {
+          tileid = 10,
+          duration = 125
+        },
+        {
+          tileid = 11,
+          duration = 125
+        },
+        {
+          tileid = 7,
+          duration = 125
+        },
+        {
+          tileid = 8,
+          duration = 125
+        },
+        {
+          tileid = 14,
+          duration = 125
+        },
+        {
+          tileid = 10,
+          duration = 125
+        },
+        {
+          tileid = 6,
+          duration = 125
+        },
+        {
+          tileid = 7,
+          duration = 125
+        },
+        {
+          tileid = 13,
+          duration = 125
+        },
+        {
+          tileid = 14,
+          duration = 125
+        }
+      }
+    },
+    {
+      id = 10,
+      animation = {
+        {
+          tileid = 10,
+          duration = 125
+        },
+        {
+          tileid = 11,
+          duration = 125
+        },
+        {
+          tileid = 7,
+          duration = 125
+        },
+        {
+          tileid = 8,
+          duration = 125
+        },
+        {
+          tileid = 14,
+          duration = 125
+        },
+        {
+          tileid = 10,
+          duration = 125
+        },
+        {
+          tileid = 6,
+          duration = 125
+        },
+        {
+          tileid = 7,
+          duration = 125
+        },
+        {
+          tileid = 13,
+          duration = 125
+        },
+        {
+          tileid = 14,
+          duration = 125
+        },
+        {
+          tileid = 5,
+          duration = 125
+        },
+        {
+          tileid = 6,
+          duration = 125
+        },
+        {
+          tileid = 12,
+          duration = 125
+        },
+        {
+          tileid = 13,
+          duration = 125
+        },
+        {
+          tileid = 9,
+          duration = 125
+        },
+        {
+          tileid = 5,
+          duration = 125
+        },
+        {
+          tileid = 11,
+          duration = 125
+        },
+        {
+          tileid = 12,
+          duration = 125
+        },
+        {
+          tileid = 8,
+          duration = 125
+        },
+        {
+          tileid = 9,
+          duration = 125
+        }
+      }
+>>>>>>> Stashed changes
     }
   }
 }
--- scripts/world/tilesets/WatersourceAnimated.tsx
+++ scripts/world/tilesets/WatersourceAnimated.tsx
@@ -1,10 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
+<<<<<<< Updated upstream
 <tileset version="1.10" tiledversion="1.10.2" name="WatersourceAnimated" tilewidth="20" tileheight="20" tilecount="10" columns="5" tilerendersize="grid">
  <editorsettings>
   <export target="WatersourceAnimated.lua" format="lua"/>
  </editorsettings>
  <image source="../../../assets/sprites/tilesets/WatersourceAnimated.png" width="100" height="40"/>
  <tile id="5">
+=======
+<tileset version="1.10" tiledversion="1.11.2" name="WatersourceAnimated" tilewidth="20" tileheight="20" tilecount="15" columns="5" tilerendersize="grid">
+ <editorsettings>
+  <export target="WatersourceAnimated.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/WatersourceAnimated.png" width="100" height="60"/>
+ <tile id="3">
+>>>>>>> Stashed changes
   <animation>
    <frame tileid="5" duration="125"/>
    <frame tileid="6" duration="125"/>
@@ -13,4 +22,64 @@
    <frame tileid="9" duration="125"/>
   </animation>
  </tile>
+<<<<<<< Updated upstream
+=======
+ <tile id="4">
+  <animation>
+   <frame tileid="10" duration="125"/>
+   <frame tileid="11" duration="125"/>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="13" duration="125"/>
+   <frame tileid="14" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="125"/>
+   <frame tileid="6" duration="125"/>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="13" duration="125"/>
+   <frame tileid="9" duration="125"/>
+   <frame tileid="5" duration="125"/>
+   <frame tileid="11" duration="125"/>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="8" duration="125"/>
+   <frame tileid="9" duration="125"/>
+   <frame tileid="10" duration="125"/>
+   <frame tileid="11" duration="125"/>
+   <frame tileid="7" duration="125"/>
+   <frame tileid="8" duration="125"/>
+   <frame tileid="14" duration="125"/>
+   <frame tileid="10" duration="125"/>
+   <frame tileid="6" duration="125"/>
+   <frame tileid="7" duration="125"/>
+   <frame tileid="13" duration="125"/>
+   <frame tileid="14" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="125"/>
+   <frame tileid="11" duration="125"/>
+   <frame tileid="7" duration="125"/>
+   <frame tileid="8" duration="125"/>
+   <frame tileid="14" duration="125"/>
+   <frame tileid="10" duration="125"/>
+   <frame tileid="6" duration="125"/>
+   <frame tileid="7" duration="125"/>
+   <frame tileid="13" duration="125"/>
+   <frame tileid="14" duration="125"/>
+   <frame tileid="5" duration="125"/>
+   <frame tileid="6" duration="125"/>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="13" duration="125"/>
+   <frame tileid="9" duration="125"/>
+   <frame tileid="5" duration="125"/>
+   <frame tileid="11" duration="125"/>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="8" duration="125"/>
+   <frame tileid="9" duration="125"/>
+  </animation>
+ </tile>
+>>>>>>> Stashed changes
 </tileset>
